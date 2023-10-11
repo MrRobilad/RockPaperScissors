1aac98e64b819e67ce9ac844b36a1758d50748e8
Dalibor Filipovski <49033327+MrRobilad@users.noreply.github.com>
2023-10-11 18:50:57 +0300

    Add files via upload

---
+++ rockpaperscissors.py
@@ -0,0 +1,55 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_images = [rock, paper, scissors]
+play_again = True  # Set to True to start the game
+
+while play_again:
+    user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.\n"))
+    print(game_images[user_choice])
+
+    computer_choice = random.randint(0, 2)
+    print("Computer chose:")
+    print(game_images[computer_choice])
+
+    if user_choice >= 3 or user_choice < 0:
+        print("You typed an invalid number, you lose!")
+    elif user_choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif computer_choice == 0 and user_choice == 2:
+        print("You lose")
+    elif computer_choice > user_choice:
+        print("You lose")
+    elif user_choice > computer_choice:
+        print("You win!")
+    elif computer_choice == user_choice:
+        print("It's a draw")
+
+    play_again_input = input("Do you want to play again? Type 'yes' or 'no': ").lower()
+    play_again = play_again_input == 'yes'
